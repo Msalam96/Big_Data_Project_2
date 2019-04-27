271f7269c3464846421a7955998a5eb41b34e994
Mohammed Salam <34826407+Msalam96@users.noreply.github.com>
2019-04-26 23:06:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ rdd = rdd.map(lambda x:(x[1],x[0]))
 for x in range(len(docsize)):
 	print("\tDoc",x+1, end='')
 
-print('\n')
+print('')
 #rdd = file.filter(lambda x:x!=' ').map(lambda word:(word.lower(),1).split()).reduceByKey(lambda x,y:x+y)
 for xs in rdd.values().take(7):
 	print(xs)
