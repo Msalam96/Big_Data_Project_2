f018726f136c1bb6ff5c26a03653939b0dc08f00
Mohammed Salam <34826407+Msalam96@users.noreply.github.com>
2019-04-26 23:02:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+from __future__ import print_function
 from pyspark import SparkContext, SparkConf
 import sys
 
 sc = SparkContext()
 
+with open('text.txt', 'r') as f:
+	docsize = f.read().splitlines()
+
 file = sc.textFile('text.txt')
-rdd = file.flatMap(lambda x:x.split())
+rdd = file.flatMap(lambda x:x.split(' '))
 rdd = rdd.map(lambda x: (x,1))
 rdd = rdd.reduceByKey(lambda x,y: x+y)
 rdd = rdd.map(lambda x:(x[1],x[0]))
-#rdd = file.filter(lambda x:x!=' ').map(lambda word:(word.lower(),1).split()).reduceByKey(lambda x,y:x+y)
-for xs in rdd.take(5):
-	print(xs)
 
+for x in range(len(docsize)):
+	print("\tDoc",x+1, end='')
+
+print('\n')
+#rdd = file.filter(lambda x:x!=' ').map(lambda word:(word.lower(),1).split()).reduceByKey(lambda x,y:x+y)
+for xs in rdd.values().take(7):
+	print(xs)
